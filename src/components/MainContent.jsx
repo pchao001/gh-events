1e604c7f3dbd0fc926c22e9b75a1d065666b7a23
LAPTOP-SV9R2L8C\pchao <pchao00@yahoo.com>
2023-04-30 22:14:35 +0800

    test

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -8,7 +8,7 @@ function MainContent() {
   function toggleHelp() {
     setHelpVisible((isVisible) => !isVisible);
   }
-
+  // test
   return (
     <main>
       <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
